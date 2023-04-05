8c5b642ca9c8e255df40798185d15fc090af5cd2
Rajeev <Rajeev@DESKTOP-7G0THSI>
2023-04-05 13:14:04 +0530

    new class

---
+++ src/test/java/module1/test03.java
@@ -0,0 +1,11 @@
+package module1;
+
+public class test03 {
+
+	public static void main(String[] args) {
+		System.out.println("poll SCM");
+		System.out.println("on demand");
+		System.out.println("on scheduling");
+	}
+
+}
