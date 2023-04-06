a6a89e83239bc6e5f1042c8e5586e630c9d01283
Rajeev <Rajeev@DESKTOP-7G0THSI>
2023-04-06 16:29:20 +0530

    newlass

---
+++ src/test/java/module1/classonne.java
@@ -0,0 +1,20 @@
+package module1;
+
+public class classonne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String UN=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(UN);
+		System.out.println(PASSWORD);
+		
+		
+		
+	}
+
+}
